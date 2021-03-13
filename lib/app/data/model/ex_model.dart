ff7e45f91c8becfeff14f3105e34cedae3a16898
jokerjamil007@gmail.com <jokerjamil007@gmail.com>
2021-03-13 13:25:43 +0600

    translation added

---
+++ lib/app/data/model/ex_model.dart
@@ -0,0 +1,20 @@
+class ExModel {
+  int id;
+  String title;
+  String body;
+
+  ExModel({id, title, body});
+
+  ExModel.fromJson(Map<String, dynamic> json) {
+    this.id = json['id'];
+    this.title = json['title'];
+    this.body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.title;
+    data['body'] = this.body;
+    return data;
+  }
+}
---
+++ lib/app/routes/ex_pages.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+import 'ex_routes.dart';
+
+class AppPages {
+  static final pages = [
+    GetPage(
+      name: Routes.INITIAL,
+      // page: () => HomePage(),
+    ),
+    GetPage(
+      name: Routes.EXAMPLE,
+      // page: () => ExamplePage(),
+      // binding: ExampleBinding(),
+    ),
+  ];
+}
